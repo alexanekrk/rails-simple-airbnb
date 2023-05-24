95ab3ae53819ce7802f923570054b867d53ccd5b
Alexane Kurek <alexane.kurek7@gmail.com>
2023-05-24 16:54:26 -0400

    index display

--- Gemfile
+++ Gemfile
@@ -15,8 +15,11 @@ gem "sqlite3", "~> 1.4"
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
 
+gem "simple_form"
+
 # Use JavaScript with ESM import maps [https://github.com/rails/importmap-rails]
 gem "importmap-rails"
+gem "faker"
 
 # Hotwire's SPA-like page accelerator [https://turbo.hotwired.dev]
 gem "turbo-rails"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning the db..."
+Flat.destroy_all
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+puts "Creating 4 flats..."
+
+4.times do
+  flat = Flat.create!(
+    name: Faker::Beer.brand,
+    address: Faker::Address.street_address,
+    description: Faker::Quote.yoda,
+    price_per_night: rand(50..500),
+    number_of_guests: rand(1..6)
+  )
+  puts "Flat with id #{flat.id} was created"
+end
+
+puts "All done!"
